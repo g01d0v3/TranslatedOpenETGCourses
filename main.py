c71406b765b29332f2da74a35154bf4040127620
g01d0v3 <nt8522247@gmail.com>
2022-09-06 00:00:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from deep_translator import GoogleTranslator
 
 browser = webdriver.Chrome()
 browser.get('https://edube.org')
-strong = 31
-def DataProcessing():
-    ForP = browser.find_elements(By.TAG_NAME, 'p')
-    ForStrong = browser.find_elements(By.TAG_NAME, 'strong')
-    for p in ForP:
-        print(p.text)
-    for strong in ForStrong:
-        print(strong.text)
-    WaitForSwitchSite()
+ForAll = browser.find_elements(By.XPATH, "//*[text()]")
+
+def translator(text):
+    translated = GoogleTranslator(source="en", target="ru").translate(text)
+    print(translated)
+    return translated
 
 def WaitForSwitchSite():
     url = browser.current_url
@@ -21,4 +19,14 @@ def WaitForSwitchSite():
         time.sleep(6)
     print ('Site Switched to ', browser.current_url)
 
+def DataProcessing():
+    for all in ForAll:
+        i=0
+        for one in str(all.text).split(sep='\n'):
+            i+=1
+            print(one)
+            browser.execute_script(f"var e = arguments[0]; e.innerText = '{one}\n {translator(one)}'", str(all.text).split(sep='\n')[i])
+        WaitForSwitchSite()
 DataProcessing()
+
+
