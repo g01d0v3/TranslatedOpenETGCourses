c36dc49284cdd309204b10b3745368711e67bca4
g01d0v3 <nt8522247@gmail.com>
2022-09-08 23:42:42 +0300

    feat(main): add methods(login, process_data, find_elements) refactor(main): Refactor DataProcessing->process_data, WaitForSwitchSite->wait_for_switch_site, translator->__translate_text add(setts): Add password and mail variables

--- main.py
+++ main.py
@@ -2,31 +2,62 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 from deep_translator import GoogleTranslator
-
-browser = webdriver.Chrome()
-browser.get('https://edube.org')
-ForAll = browser.find_elements(By.XPATH, "//*[text()]")
-
-def translator(text):
-    translated = GoogleTranslator(source="en", target="ru").translate(text)
-    print(translated)
-    return translated
-
-def WaitForSwitchSite():
-    url = browser.current_url
-    while browser.current_url == url:
-        print(url)
-        time.sleep(6)
-    print ('Site Switched to ', browser.current_url)
-
-def DataProcessing():
-    for all in ForAll:
-        i=0
-        for one in str(all.text).split(sep='\n'):
-            i+=1
-            print(one)
-            browser.execute_script(f"var e = arguments[0]; e.innerText = '{one}\n {translator(one)}'", str(all.text).split(sep='\n')[i])
-        WaitForSwitchSite()
-DataProcessing()
+import setts
 
 
+class EdubeTranslator():
+    def __init__(self):
+        self.browser = webdriver.Chrome()
+        self.browser.get('https://edube.org/login')
+
+    def find_elements(self):
+        ForAll = self.browser.find_elements(By.TAG_NAME, 'p') + self.browser.find_elements(By.TAG_NAME, 'strong') + self.browser.find_elements(By.TAG_NAME, 'li')
+        return ForAll
+
+    def login(self):
+        if setts.mail and setts.password:
+            if input('Wanna use recent data(y/n)?\n').lower() == "y":
+                mail, password = setts.mail, setts.password
+            else:
+                mail = input("ur mail ")
+                password = input("ur password ")
+        else:
+            mail = input("ur mail ")
+            password = input("ur password ")
+        self.browser.find_element(By.ID, "email").send_keys(mail)
+        self.browser.find_element(By.ID, "password").send_keys(password)
+        self.browser.find_element(By.XPATH, "//*[@id='content-wrap']/div/div/div/div/form/button").click()
+        time.sleep(2)
+        self.browser.get('https://edube.org/')
+
+    def __translate_text(self, text):
+        translated = GoogleTranslator(source="en", target="ru").translate(text)
+        print(translated)
+        return translated
+
+    def wait_for_switch_site(self):
+        url = self.browser.current_url
+        while self.browser.current_url == url:
+            print(url)
+            time.sleep(6)
+        print('Site Switched to ', self.browser.current_url)
+        return True
+
+    def process_data(self):
+        if self.find_elements():
+            for all in self.find_elements():
+                print(all.text)
+                try:
+                    self.browser.execute_script(
+                        f"var e = arguments[0]; e.insertAdjacentHTML('beforeend', '<br>{str(self.__translate_text(all.text))}')",
+                        all)
+                except Exception as e:
+                    print('woops, some error stack process: ', e)
+
+
+if __name__ == "__main__":
+    Chrome = EdubeTranslator()
+    Chrome.login()
+    Chrome.process_data()
+    while Chrome.wait_for_switch_site():
+        Chrome.process_data()
